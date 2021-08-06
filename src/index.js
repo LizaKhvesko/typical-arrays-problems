7b03793b9e332ffde9148cbce8ee3c3d0975a56f
LizaKhvesko <khveskoliza@tut.by>
2021-08-06 13:59:56 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  else {
+      let sort = array.sort((a,b) => b-a);
+    return sort[sort.length-1];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  else {
+      let sort = array.sort((a,b) => b-a);
+    return sort[0];
+  }
 }
-
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  else {
+    let reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return array.reduce(reducer)/array.length;
+  }
 }
